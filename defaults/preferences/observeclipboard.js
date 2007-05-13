8356a42e67abb9b5bde1c059bcc931bf7eae64b9
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-05-13 20:37:41 +0000

    拡張機能名と説明文をローカライズした
    git-svn-id: https://www.cozmixng.org/repos/piro/observeclipboard/trunk@570 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ defaults/preferences/observeclipboard.js
@@ -0,0 +1,2 @@
+pref("extensions.{84BE9FF4-6D4F-4477-8E8A-86CF17F053BA}.name", "chrome://observeclipboard/locale/observeclipboard.properties");
+pref("extensions.{84BE9FF4-6D4F-4477-8E8A-86CF17F053BA}.description", "chrome://observeclipboard/locale/observeclipboard.properties");
