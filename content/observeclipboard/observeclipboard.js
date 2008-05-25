f00279fa8152cd7eb59bb80d967986c1c0349f6b
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-05-25 19:18:08 +0000

    TextLink互換の動作にした
    git-svn-id: https://www.cozmixng.org/repos/piro/observeclipboard/trunk@2539 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- content/observeclipboard/observeclipboard.js
+++ content/observeclipboard/observeclipboard.js
@@ -46,7 +46,7 @@ var ClipboardObserverService = {
 	PREFROOT : 'extensions.{84BE9FF4-6D4F-4477-8E8A-86CF17F053BA}',
 
 	knsISupportsString : ('nsISupportsWString' in Components.interfaces) ? Components.interfaces.nsISupportsWString : Components.interfaces.nsISupportsString,
-	
+	 
 	// properties 
 	
 	get IOService() 
@@ -90,44 +90,8 @@ var ClipboardObserverService = {
 		return this._Clipboard;
 	},
 	_Clipboard : null,
-	
-	getURIFromClipboard : function() 
-	{
-		var uris = this.getURIsFromClipboard();
-		return uris.length ? uris[0] : null ;
-	},
-	getURIsFromClipboard : function()
-	{
-		var res = [];
-		try {
-			var str = this.getClipboardContent();
-			if (!str) return res;
-
-			// is the string an URI?
-			var uris = str.match(/([-+a-z0-9.]+:\/\/|[-a-z0-9]+(\.[-a-z0-9]+)+)[-_.!~*'()a-z0-9;\/?:\@&=+\$,%#]+/gi);
-			if (!uris.length) return res;
-
-			for (var i in uris)
-			{
-				if (uris[i].constructor == 'Array')
-					uris[i] = uris[i][0];
-				else if (typeof uris[i] != 'string')
-					uris[i] = String(uris[i]);
-
-				if (uris[i].match(/^(\w+:\/\/|urn:|www[0-9]*\.)/)/* ||
-					uris[i].match(/[-a-z0-9.]\.(com|org|net|edu|gov|mil|(co|or|ne|ed|ac|ad|go|mi)\.[a-z]{2})/i)*/)
-					res.push(uris[i]
-							.replace(/^(www[0-9]*\.)/, 'http://$1')
-							.replace(/^(ftp\.)/, 'http://$1')
-							.replace(/^h?t?t?p:\/\//, 'http://')
-							.replace(/^h?t?t?ps:\/\//, 'https://'));
-			}
-		}
-		catch(e) {
-		}
-		return res;
-	},
-	getClipboardContent : function()
+	 
+	getClipboardContent : function() 
 	{
 		var str = '';
 
@@ -243,6 +207,235 @@ var ClipboardObserverService = {
 		return (Components.lookupMethod(window, 'top').call(window).document.documentElement.getAttribute('windowtype') == 'navigator:browser');
 	},
   
+	// URI manipulation (from textlink) 
+	 
+	get kURIPattern() 
+	{
+		if (!this._kURIPattern)
+			this._kURIPattern = this.kURIPattern_base.replace(
+					/%PART_PATTERN%/g,
+					this.kURIPattern_part
+				).replace(
+					/%DOMAIN_PATTERN%/g,
+					'[0-9a-z\\.-]+\\.('+this.kTopLevelDomains.join('|')+')'
+				);
+
+		return this._kURIPattern;
+	},
+	_kURIPattern : null,
+ 
+	get kURIPatternMultibyte() 
+	{
+		if (!this._kURIPatternMultibyte)
+			this._kURIPatternMultibyte = this.kURIPatternMultibyte_base.replace(
+					/%PART_PATTERN%/g,
+					this.kURIPatternMultibyte_part
+				).replace(
+					/%DOMAIN_PATTERN%/g,
+					'[0-9a-z\\.-]+[\\.\uff0e]('+this.kTopLevelDomains.join('|')+')'
+/*
+					'[0-9a-z\\.-\uff10-\uff19\uff41-\uff5a\uff21-\uff3a\uff0e\uff0d]+[\\.\uff0e]('+
+					this.kTopLevelDomains.join('|')+
+					'|'+
+					this.convertHalfWidthToFullWidth(this.kTopLevelDomains.join('|')).replace(/\uff5c/g, '|')+
+					')'
+*/
+				);
+
+		return this._kURIPatternMultibyte;
+	},
+	_kURIPatternMultibyte : null,
+ 
+	kURIPattern_base : '\\(?([\\*\\+\\w]+:(//)?%PART_PATTERN%|%DOMAIN_PATTERN%(/%PART_PATTERN%)?)', 
+	kURIPatternMultibyte_base : '[\\(\uff08]?([\\*\\+a-z0-9_\uff41-\uff5a\uff21-\uff3a\uff10-\uff19\uff3f]+[:\uff1a](//|\uff0f\uff0f)?%PART_PATTERN%|%DOMAIN_PATTERN%([/\uff0f]%PART_PATTERN%)?)',
+
+	kURIPattern_part : '[-_\\.!~*\'()a-z0-9;/?:@&=+$,%#]+',
+	kURIPatternMultibyte_part : '[-_\\.!~*\'()a-z0-9;/?:@&=+$,%#\uff0d\uff3f\uff0e\uff01\uff5e\uffe3\uff0a\u2019\uff08\uff09\uff41-\uff5a\uff21-\uff3a\uff10-\uff19\uff1b\uff0f\uff1f\uff1a\uff20\uff06\uff1d\uff0b\uff04\uff0c\uff05\uff03]+',
+
+	kOnebyteArray : '-_.!~~*\'()acdefghijklmnopqrstuvwxyzACDEFGHIJKLMNOPQRSTUVWXYZ0123456789;/?:@&=+$,%#',
+	kMultibyteArray : '\uff0d\uff3f\uff0e\uff01\uffe3\uff5e\uff0a\u2019\uff08\uff09\uff41\uff43\uff44\uff45\uff46\uff47\uff48\uff49\uff4a\uff4b\uff4c\uff4d\uff4e\uff4f\uff50\uff51\uff52\uff53\uff54\uff55\uff56\uff57\uff58\uff59\uff5a\uff21\uff23\uff24\uff25\uff26\uff27\uff28\uff29\uff2a\uff2b\uff2c\uff2d\uff2e\uff2f\uff30\uff31\uff32\uff33\uff34\uff35\uff36\uff37\uff38\uff39\uff3a\uff10\uff11\uff12\uff13\uff14\uff15\uff16\uff17\uff18\uff19\uff1b\uff0f\uff1f\uff1a\uff20\uff06\uff1d\uff0b\uff04\uff0c\uff05\uff03',
+
+	// see http://www4.plala.or.jp/nomrax/TLD/
+	kTopLevelDomains : [
+		// iTLD , gTLD
+		'arpa', 'int', 'nato', 'com', 'net', 'org', 'info', 'biz', 'name', 'pro', 'museum', 'coop', 'aero', 'edu', 'gov', 'mil',
+
+		// ccTLD
+		'ac', 'ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'an', 'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az',
+		'ba', 'bb', 'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bm', 'bn', 'bo', 'br', 'bs', 'bt', 'bu', 'bv', 'bw', 'by', 'bz',
+		'ca', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl', 'cm', 'cn', 'co', 'cp', 'cr', 'cs', 'sk', 'cu', 'cv', 'cx', 'cy', 'cz',
+		'dd', 'de', 'dg', 'dj', 'dk', 'dm', 'do', 'dz',
+		'ea', 'ec', 'ee', 'eg', 'eh', 'er', 'es', 'et',
+		'fi', 'fj', 'fk', 'fm', 'fo', 'fr', 'fx',
+		'ga', 'gb', 'gd', 'ge', 'gf', 'gg', 'gh', 'gi', 'gl', 'gm', 'gn', 'gp', 'gq', 'gr', 'gs', 'gt', 'gu', 'gw', 'gy',
+		'hk', 'hm', 'hn', 'hr', 'ht', 'hu',
+		'ic', 'id', 'ie', 'il', 'im', 'in', 'io', 'iq', 'ir', 'is', 'it',
+		'je', 'jm', 'jo', 'jp',
+		'ke', 'kg', 'kh', 'ki', 'km', 'kn', 'kp', 'kr', 'kw', 'ky', 'kz',
+		'la', 'lb', 'lc', 'li', 'lk', 'lr', 'ls', 'lt', 'lu', 'lv', 'ly',
+		'ma', 'mc', 'md', 'mg', 'mh', 'mk', 'ml', 'mm', 'mn', 'mo', 'mp', 'mq', 'mr', 'ms', 'mt', 'mu', 'mv', 'mw', 'mx', 'my', 'mz',
+		'na', 'nc', 'ne', 'nf', 'ng', 'ni', 'nl', 'no', 'np', 'nr', 'nt', 'nu', 'nz',
+		'om',
+		'pa', 'pc', 'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pm', 'pn', 'pr', 'ps', 'pt', 'pw', 'py', 'qa',
+		're', 'ro', 'ru', 'rw',
+		'sa', 'sb', 'sc', 'sd', 'se', 'sg', 'sh', 'si', 'sj', 'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'st', 'su', 'sv', 'sy', 'sz',
+		'ta', 'tc', 'td', 'tf', 'tg', 'th', 'tj', 'tk', 'tm', 'tn', 'to', 'tp', 'tr', 'tt', 'tv', 'tw', 'tz',
+		'ua', 'ug', 'uk', 'um', 'us', 'uy', 'uz',
+		'va', 'vc', 've', 'vg', 'vi', 'vn', 'vu',
+		'wf', 'wg', 'ws',
+		'yd', 'ye', 'yt', 'yu',
+		'za', 'zm', 'zr', 'zw'
+	],
+ 
+	// from http://taken.s101.xrea.com/blog/article.php?id=510
+	convertFullWidthToHalfWidth : function(aString) 
+	{
+		return aString.replace(this.fullWidthRegExp, this.f2h);
+	},
+	fullWidthRegExp : /[\uFF01\uFF02\uFF03\uFF04\uFF05\uFF06\uFF07\uFF08\uFF09\uFF0A\uFF0B\uFF0C\uFF0D\uFF0E\uFF0F\uFF10\uFF11\uFF12\uFF13\uFF14\uFF15\uFF16\uFF17\uFF18\uFF19\uFF1A\uFF1B\uFF1C\uFF1D\uFF1E\uFF1F\uFF20\uFF21\uFF22\uFF23\uFF24\uFF25\uFF26\uFF27\uFF28\uFF29\uFF2A\uFF2B\uFF2C\uFF2D\uFF2E\uFF2F\uFF30\uFF31\uFF32\uFF33\uFF34\uFF35\uFF36\uFF37\uFF38\uFF39\uFF3A\uFF3B\uFF3C\uFF3D\uFF3E\uFF3F\uFF40\uFF41\uFF42\uFF43\uFF44\uFF45\uFF46\uFF47\uFF48\uFF49\uFF4A\uFF4B\uFF4C\uFF4D\uFF4E\uFF4F\uFF50\uFF51\uFF52\uFF53\uFF54\uFF55\uFF56\uFF57\uFF58\uFF59\uFF5A\uFF5B\uFF5C\uFF5D\uFF5E]/g,
+	f2h : function() {
+		var str = arguments[0];
+		var code = str.charCodeAt(0);
+		code &= 0x007F;
+		code += 0x0020;
+		return String.fromCharCode(code);
+	},
+
+	convertHalfWidthToFullWidth : function(aString)
+	{
+		return aString.replace(this.halfWidthRegExp, this.h2f);
+	},
+	halfWidthRegExp : /[!"#$%&'\(\)\*\+,-\.\/0123456789:;<=>\?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\[\\\]\^_`abcdefghijklmnopqrstuvwxyz\{\|\}~]/g,
+	h2f : function() {
+		var str = arguments[0];
+		var code = str.charCodeAt(0);
+		code += 0xFF00;
+		code -= 0x0020;
+		return String.fromCharCode(code);
+	},
+ 
+	matchURIRegExp : function(aString) 
+	{
+		var regexp = [];
+		if (this.getPref('observeclipboard.multibyte.enabled')) {
+			regexp.push(this.kURIPatternMultibyte);
+		}
+		else {
+			regexp.push(this.kURIPattern);
+		}
+		return aString.match(new RegExp(regexp.join('|'), 'ig'));
+	},
+ 
+	fixupURI : function(aURIComponent) 
+	{
+		if (this.getPref('observeclipboard.multibyte.enabled')) {
+			aURIComponent = this.convertFullWidthToHalfWidth(aURIComponent);
+		}
+
+		aURIComponent = this.sanitizeURIString(aURIComponent);
+		if (!aURIComponent) return null;
+
+		aURIComponent = this.fixupSchemer(aURIComponent);
+
+		var regexp = new RegExp();
+		if (
+			regexp.compile(
+				'('+
+				this.getPref('observeclipboard.schemer')
+						.replace(/([\(\)\+\.\{\}])/g, '\\$1')
+						.replace(/\?/g, '.')
+						.replace(/\*/g, '.+')
+						.replace(/[,\| \n\r\t]+/g, '|')+
+				')'
+			).test(
+				aURIComponent.substr(0, aURIComponent.indexOf(':')).toLowerCase()
+			)
+			)
+			return aURIComponent;
+
+		return null;
+	},
+	 
+	sanitizeURIString : function(aURIComponent) 
+	{
+		while (
+			aURIComponent.match(/^\((.*)$/) ||
+			aURIComponent.match(/^([^\(]*)\)$/) ||
+			aURIComponent.match(/^(.*)[\.,]$/) ||
+			aURIComponent.match(/^([^\"]*)\"$/) ||
+			aURIComponent.match(/^([^\']*)\'$/) ||
+			aURIComponent.match(/^(.+)\s*\([^\)]+$/) ||
+			aURIComponent.match(/^[^\(]+\)\s*(.+)$/) ||
+			aURIComponent.match(/^[^\.\/:]*\((.+)\)[^\.\/]*$/)
+			)
+			aURIComponent = RegExp.$1;
+
+		return aURIComponent; // aURIComponent.replace(/^.*\((.+)\).*$/, '$1');
+	},
+ 
+	fixupSchemer : function(aURI) 
+	{
+		var table = this.getPref('observeclipboard.schemer.fixup.table')
+						.replace(/(\s*[^:\s]+)\s*=>\s*([^:\s]+)(\s*([,\| \n\r\t]|$))/g, '$1:=>$2:$3');
+		var regexp = new RegExp();
+
+		var targets = table.replace(/\s*=>\s*[^,\| \n\r\t]+|\s*=>\s*[^,\| \n\r\t]+$/g, '')
+						.replace(/([\(\)\+\.\{\}])/g, '\\$1')
+						.replace(/\?/g, '.')
+						.replace(/\*/g, '.+')
+						.replace(/\s*[,\| \n\r\t]+\s*/g, '|');
+
+		if (aURI.match(regexp.compile('^('+targets+')', 'g'))) {
+			var target = RegExp.$1;
+			eval((targets+'|')
+					.replace(/([^|]+)\|/g,
+						'if (/^$1$/.test("'+target+'")) table = table.replace(/\\b$1\\s*=>/, "'+target+'=>");'
+				));
+
+			if (table.match(
+					regexp.compile(
+						'([,\\| \\n\\r\\t]|^)'+
+						target.replace(/([\(\)\+\?\.\{\}])/g, '\\$1')
+							.replace(/\?/g, '.')
+							.replace(/\*/g, '.+')+
+						'\\s*=>\\s*([^,\\| \\n\\r\\t]+)'
+					)
+				))
+				aURI = aURI.replace(target, RegExp.$2);
+		}
+
+		return aURI;
+	},
+   
+	getURIsFromClipboard : function() 
+	{
+		var res = [];
+		try {
+			var str = this.getClipboardContent();
+			if (!str) return res;
+
+			// is the string an URI?
+			var uris = this.matchURIRegExp(str);
+			if (!uris.length) return res;
+
+			var uri;
+			for (var i in uris)
+			{
+				uri = this.fixupURI(uris[i]);
+				if (uri) res.push(uri);
+			}
+		}
+		catch(e) {
+		}
+		return res;
+	},
+ 
+	getURIFromClipboard : function() 
+	{
+		var uris = this.getURIsFromClipboard();
+		return uris.length ? uris[0] : null ;
+	},
+ 	
 	init : function() 
 	{
 		if (this.activated) return;
@@ -347,7 +540,7 @@ var ClipboardObserverService = {
 
 		eval(this.readFromURI('chrome://observeclipboard/content/default.js'));
 	},
-	
+	 
 	readFromURI : function(aURI) 
 	{
 		try {
